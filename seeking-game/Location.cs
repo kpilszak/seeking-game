81d42b89a79fce569f11c122e20372c7597a9790
kpilszak <klaudia.pilszak@gmail.com>
2018-12-29 17:40:33 +0100

    Create Location.cs

---
+++ seeking-game/Location.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seeking_game
+{
+    abstract class Location
+    {
+        public Location(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+        public Location[] Exits;
+
+        public virtual string Description
+        {
+            get
+            {
+                string description = "You are in " + Name + ". You can see entries to following locations: ";
+                for (int i = 0; i < Exits.Length; i++)
+                {
+                    description += " " + Exits[i].Name;
+                    if (i != Exits.Length - 1)
+                        description += ", ";
+                }
+                description += ".";
+                return description;
+            }
+        }
+    }
+}
