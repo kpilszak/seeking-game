011f05e93f81bd13425e59a3874904aa94885ff8
kpilszak <klaudia.pilszak@gmail.com>
2018-12-30 14:14:48 +0100

    Create Outside.cs

---
+++ seeking-game/Outside.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seeking_game
+{
+    class Outside : Location
+    {
+        public Outside(string name, bool hot)
+            : base(name)
+        {
+            this.hot = hot;
+        }
+
+        private bool hot;
+
+        public override string Description
+        {
+            get
+            {
+                string NewDescription = base.Description;
+                if (hot)
+                    NewDescription += " It's very hot here.";
+                return NewDescription;
+            }
+        }
+    }
+}
