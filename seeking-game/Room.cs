17ea59b070b5e71667981177c58d5f2b69d8eeaf
kpilszak <klaudia.pilszak@gmail.com>
2018-12-30 14:12:13 +0100

    Create Room.cs

---
+++ seeking-game/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seeking_game
+{
+    class Room : Location
+    {
+        public Room(string name, string decoration)
+            : base(name)
+        {
+            this.decoration = decoration;
+        }
+
+        private string decoration;
+
+        public override string Description
+        {
+            get
+            {
+                return base.Description + " Here you can see " + decoration + ".";
+            }
+        }
+
+    }
+}
