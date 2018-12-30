c6f2017b0869d7ddfd47e86d6465d4562b3fb3cb
kpilszak <klaudia.pilszak@gmail.com>
2018-12-30 15:42:08 +0100

    Create RoomWithHidingPlace.cs

---
+++ seeking-game/RoomWithHidingPlace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seeking_game
+{
+    class RoomWithHidingPlace : Room, IHidingPlace
+    {
+        public RoomWithHidingPlace(string name, string decoration, string hidingPlaceName)
+            : base(name, decoration)
+        {
+            HidingPlaceName = hidingPlaceName;
+        }
+
+        public string HidingPlaceName { get; private set; }
+
+        public override string Description
+        {
+            get
+            {
+                return base.Description + " Someone is possible to hide in " + HidingPlaceName + ".";
+            }
+        }
+    }
+}
