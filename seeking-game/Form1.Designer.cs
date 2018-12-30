514a3221eb6e1ed4d9a9a00d48959fc9895c908f
kpilszak <klaudia.pilszak@gmail.com>
2018-12-30 14:53:47 +0100

    Update Form1.Designer.cs

--- seeking-game/Form1.Designer.cs
+++ seeking-game/Form1.Designer.cs
@@ -28,19 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.description = new System.Windows.Forms.TextBox();
+            this.goHere = new System.Windows.Forms.Button();
+            this.exits = new System.Windows.Forms.ComboBox();
+            this.goThroughTheDoor = new System.Windows.Forms.Button();
+            this.check = new System.Windows.Forms.Button();
+            this.hide = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(13, 13);
+            this.description.Multiline = true;
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(645, 286);
+            this.description.TabIndex = 0;
+            // 
+            // goHere
+            // 
+            this.goHere.Location = new System.Drawing.Point(13, 306);
+            this.goHere.Name = "goHere";
+            this.goHere.Size = new System.Drawing.Size(207, 43);
+            this.goHere.TabIndex = 1;
+            this.goHere.Text = "Go here:";
+            this.goHere.UseVisualStyleBackColor = true;
+            // 
+            // exits
+            // 
+            this.exits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.exits.FormattingEnabled = true;
+            this.exits.Items.AddRange(new object[] {
+            "Garden",
+            "Front Yard",
+            "Back Yard",
+            "Living room",
+            "Dining Room",
+            "Kitchen"});
+            this.exits.Location = new System.Drawing.Point(227, 314);
+            this.exits.Name = "exits";
+            this.exits.Size = new System.Drawing.Size(431, 28);
+            this.exits.TabIndex = 2;
+            // 
+            // goThroughTheDoor
+            // 
+            this.goThroughTheDoor.Location = new System.Drawing.Point(13, 356);
+            this.goThroughTheDoor.Name = "goThroughTheDoor";
+            this.goThroughTheDoor.Size = new System.Drawing.Size(645, 43);
+            this.goThroughTheDoor.TabIndex = 3;
+            this.goThroughTheDoor.Text = "Go through the door";
+            this.goThroughTheDoor.UseVisualStyleBackColor = true;
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(13, 406);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(645, 43);
+            this.check.TabIndex = 4;
+            this.check.Text = "Check";
+            this.check.UseVisualStyleBackColor = true;
+            // 
+            // hide
+            // 
+            this.hide.Location = new System.Drawing.Point(13, 456);
+            this.hide.Name = "hide";
+            this.hide.Size = new System.Drawing.Size(645, 43);
+            this.hide.TabIndex = 5;
+            this.hide.Text = "Hide!";
+            this.hide.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(670, 514);
+            this.Controls.Add(this.hide);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.goThroughTheDoor);
+            this.Controls.Add(this.exits);
+            this.Controls.Add(this.goHere);
+            this.Controls.Add(this.description);
             this.Name = "Form1";
+            this.Text = "Seeking game";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.Button goHere;
+        private System.Windows.Forms.ComboBox exits;
+        private System.Windows.Forms.Button goThroughTheDoor;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Button hide;
     }
 }
 
