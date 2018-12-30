d5759b0170aadeb7f9c89e99c2b6875637a98b8c
kpilszak <klaudia.pilszak@gmail.com>
2018-12-30 14:23:34 +0100

    Create OutsideWithDoor.cs

---
+++ seeking-game/OutsideWithDoor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seeking_game
+{
+    class OutsideWithDoor : Outside, IHasExteriorDoor
+    {
+        public OutsideWithDoor(string name, bool hot, string doorDescription)
+            : base(name, hot)
+        {
+            this.DoorDescription = doorDescription;
+        }
+
+        public Location DoorLocation { get; set; }
+        public string DoorDescription { get; private set; }
+
+        public override string Description
+        {
+            get
+            {
+                return base.Description + " Now you can see " + DoorDescription + ".";
+            }
+        }
+    }
+}
